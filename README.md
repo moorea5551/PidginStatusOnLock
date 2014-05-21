96c0c820da94b4db5e60736552732d047e5b50b6
amoore <amoore@franklinamerican.com>
2014-05-21 09:27:11 -0500

    adding script and readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ PidginStatusOnLock
 ==================
 
 Sets your pidgin status to away when the screen is locked and available when it's unlocked.  Currently works for Mate and will work for others with a simple config change.
+
+##Usage
+To use this script, run `python onLockUnlock.py` at startup or from your console.
+If you're not using Mate as your window manager, the dbus_interface will have to be changed at the bottom of the script to the interface name for your particular screensaver.  For example, if you're using gnome screensaver, it's a simple change from `org.mate.ScreenSaver` to `org.gnome.ScreenSaver`.
---
+++ onLockUnlock.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#coding: utf-8
+#vim: set et sw=2
+
+import dbus
+import dbus.glib
+import os
+import subprocess
+
+def screensaver_changed(state):
+	try:
+		proc = subprocess.Popen(["purple-remote getstatus"], stdout=subprocess.PIPE, shell=True)
+		(out, err) = proc.communicate()
+		if out == "available\n":
+			os.system("purple-remote 'setstatus?status=away'")
+		else:
+			os.system("purple-remote 'setstatus?status=available'")
+	except:
+		print "It's Borked!"
+
+def main():
+	import gobject
+	gobject.MainLoop().run()
+
+session_bus = dbus.SessionBus()
+session_bus.add_signal_receiver(screensaver_changed, signal_name='ActiveChanged', dbus_interface='org.mate.ScreenSaver')
+main()
